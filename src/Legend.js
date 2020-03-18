fc95bc76d012675a3f6314d9a86e764158775ba9
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-03-18 12:43:25 -0400

    legend minimizable

--- src/Legend.js
+++ src/Legend.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 class Legend extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { expanded: true };
+
+    this.toggleExpand = this.toggleExpand.bind(this);
+  }
+
+  toggleExpand() {
+    this.setState(state => ({ expanded: !state.expanded }));
+  }
+
   render() {
+    let classes = 'legend';
+    if (!this.state.expanded) {
+      classes += ' hidden';
+    }
     return (
-      <div className='legend'>
-          <div><i className='past' />Past Experiments</div>
-          <div><i className='ongoing' />Ongoing Experiments</div>
-          <div><i className='proposed' />Proposed Experiments</div>
+      <div className={classes}>
+          <p onClick={this.toggleExpand} className='expand-icon'>[{this.state.expanded ? '-' : '+'}]</p>
+          <div className='content'>
+            <div><i className='past' />Past Experiments</div>
+            <div><i className='ongoing' />Ongoing Experiments</div>
+            <div><i className='proposed' />Proposed Experiments</div>
+          </div>
       </div>
     )
   }
