195bb1242356f47b78a0cef2fdbef4150a7af538
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-03-18 15:44:05 -0400

    Updates

---
+++ precache-manifest.ced1ef0f5e85123964b37c23146ec316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4444c71f52c3a0c45cad0aa0af95baf",
+    "url": "/ubi-map/index.html"
+  },
+  {
+    "revision": "701c7a4e02163c8cae85",
+    "url": "/ubi-map/static/css/main.1432010b.chunk.css"
+  },
+  {
+    "revision": "cf156165ee350c46034c",
+    "url": "/ubi-map/static/js/2.d1f2fa4c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ubi-map/static/js/2.d1f2fa4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "701c7a4e02163c8cae85",
+    "url": "/ubi-map/static/js/main.f4652081.chunk.js"
+  },
+  {
+    "revision": "409bc193a3b0063f03ff",
+    "url": "/ubi-map/static/js/runtime-main.fee46c45.js"
+  }
+]);
